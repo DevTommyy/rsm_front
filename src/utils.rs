83110c7efd565bc97e78339a8d50d2280bad4467
Tommaso Desiderio <desideriotommaso@gmail.com>
2025-01-26 12:25:51 +0100

    feat: update utils for macos build

--- src/utils.rs
+++ src/utils.rs
@@ -20,18 +20,35 @@ pub fn prompt_credentials() -> io::Result<(String, String)> {
 
 /// Gets the system timezone as a `chrono_tz::Tz` instance.
 pub fn get_sys_tz() -> Option<chrono_tz::Tz> {
-    // Read the contents of /etc/timezone (common on Debian-based systems)
-    if let Ok(timezone) = fs::read_to_string("/etc/timezone") {
-        return timezone.trim().parse().ok();
+    #[cfg(target_os = "linux")]
+    {
+        // Read the contents of /etc/timezone (common on Debian-based systems)
+        if let Ok(timezone) = fs::read_to_string("/etc/timezone") {
+            return timezone.trim().parse().ok();
+        }
+
+        // Read the timezone info from /etc/localtime (common on many Linux distributions)
+        if let Ok(target) = fs::read_link("/etc/localtime") {
+            if let Some(zone) = target
+                .to_str()
+                .and_then(|path| path.split("/zoneinfo/").nth(1))
+            {
+                return zone.parse().ok();
+            }
+        }
     }
 
-    // Read the timezone info from /etc/localtime (common on many Linux distributions)
-    if let Ok(target) = fs::read_link("/etc/localtime") {
-        if let Some(zone) = target
-            .to_str()
-            .and_then(|path| path.split("/zoneinfo/").nth(1))
-        {
-            return zone.parse().ok();
+    // TODO: test this on macos
+    #[cfg(target_os = "macos")]
+    {
+        // macOS also uses /etc/localtime symlink to /var/db/timezone/zoneinfo
+        if let Ok(target) = fs::read_link("/etc/localtime") {
+            if let Some(zone) = target
+                .to_str()
+                .and_then(|path| path.split("/zoneinfo/").nth(1))
+            {
+                return zone.parse().ok();
+            }
         }
     }
 

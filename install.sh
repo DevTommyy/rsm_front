2f29adb20fadd7bc3b14d9e18310b9439a4ffdad
Tommaso Desiderio <desideriotommaso@gmail.com>
2025-02-04 11:45:08 +0100

    build(install): update install script

--- install.sh
+++ install.sh
@@ -8,5 +8,14 @@ current_dir=$(pwd)
 # Define the path to the target
 target="$current_dir/target/release/rsm_front"
 
+# Define the symlink path
+symlink_path="/usr/local/bin/rsm"
+
+# Remove existing symlink if it exists
+if [ -L "$symlink_path" ]; then
+	echo "WARNING: symlink to rsm already exists, removing it"
+	sudo rm "$symlink_path"
+fi
+
 # Create the symlink
-sudo ln -s "$target" /usr/local/bin/rsm
+sudo ln -s "$target" "$symlink_path"
